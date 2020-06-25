e2ee487f1460abe6c75fb94242abae0df1b5439d
Austin McCall <mccallau@umich.edu>
2020-06-25 16:30:14 -0400

    content followed

---
+++ nws_ps_statModelPy/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Stats Models practice with Python',
+    author='Austin McCall',
+    license='MIT',
+)
